ddce9b0a002d6a188933ff20123ed80e9e3d5b96
RIYA TYAGI <119611030+riyatyag@users.noreply.github.com>
2025-04-04 18:12:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-function toggleMobileMenu(){
-	document.getElementById("menu").classList.toggle("active");
-}
-const links = menu.querySelectorAll("a");
+<script>
+  function toggleMobileMenu() {
+    document.getElementById("menu").classList.toggle("hidden");
+  }
+
+  const menu = document.getElementById("menu");
+  const links = menu.querySelectorAll("a");
   links.forEach(link => {
     link.addEventListener("click", () => {
-      menu.classList.remove("active");
+      if (window.innerWidth < 768) {
+        menu.classList.add("hidden");
+      }
     });
   });
+</script>
